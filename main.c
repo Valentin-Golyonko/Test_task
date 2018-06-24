1f18a0f6ca56867045bd0d213c77fd633f72db16
bequite <valentin8dev@gmail.com>
2018-06-24 18:50:32 +0300

    fix bug with process and threads close

--- main.c
+++ main.c
@@ -6,12 +6,14 @@
 #include <stdlib.h>
 #include <sys/shm.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 #include "shm_b.h"
 #include "shm_c1.h"
 
 #define BUF_SIZE 10
-static int killProcesses = 0;
+
+static bool killProcesses = false;
 
 void shmC1(void) {
 
@@ -51,14 +53,15 @@ void shmC1(void) {
     shmPtrC1->status = TAKEN;
     printf("   shmC1 has informed processB data have been taken...\n");
 
-    if (killProcesses == 1) {
+    if (killProcesses) {
         shmdt((void *) shmPtrC1);
         printf("   shmC1 has detached its shared memory...\n");
 
-        if (kill(shmC1pid, SIGUSR1) == 0) {
-            printf("shmC1pid %d off\n", shmC1pid);
-        }
-        exit(EXIT_SUCCESS);
+        int k = kill(shmC1pid, SIGUSR1);
+        printf("shmC1pid %d %d\n", shmC1pid, k);
+
+        //exit(EXIT_SUCCESS);
+        pthread_exit(NULL);
     }
 }
 
@@ -67,6 +70,11 @@ void *c1Pthread(void *c1) {
     int c1pid = getpid();
     printf("   C1 pid: %d\n", c1pid);
     printf("   C1 pthread id: %lu\n", pthread_self());
+
+    if (killProcesses) {
+        puts("c1Pthread exit\n");
+        pthread_exit(NULL);
+    }
 
     shmC1();
 
@@ -89,6 +97,11 @@ void *c2Pthread(void *c2) {
     printf("   C2 pid: %d\n", c2pid);
     printf("   C2 pthread id: %lu\n", pthread_self());
 
+    if (killProcesses) {
+        puts("c2Pthread exit\n");
+        pthread_exit(NULL);
+    }
+
     for (int i = 0; i < 3600; ++i) {        // c2_thread work time = 1 hour
         puts("   doing well\n");
 
@@ -100,14 +113,16 @@ void *c2Pthread(void *c2) {
 
 void processC(void) {
 
-    pid_t cPid;
-    cPid = fork();
+    pid_t cPid = -1;
 
-    int cpid = getpid();
-    printf("  C pid: %d\n", cpid);
+    int pid = getpid();
+
+    if (cPid != pid) {
+        cPid = fork();
+    }
+    printf("  C pid: %d, pid: %d\n", pid, cPid);
 
     pthread_t thread1, thread2;
-    //int iret1, iret2;
 
     if (cPid == -1) {
         perror("  processC fork failed\n");
@@ -121,6 +136,7 @@ void processC(void) {
             printf("  threadC1 error\n");
             exit(EXIT_FAILURE);
         }
+
         if (pthread_create(&thread2, NULL, c2Pthread, NULL)) {
             printf("  threadC2 error\n");
             exit(EXIT_FAILURE);
@@ -130,17 +146,19 @@ void processC(void) {
             printf("  error join threadC1\n");
             exit(EXIT_FAILURE);
         }
+
         if (pthread_join(thread2, NULL)) {
             printf("  error join threadC2\n");
             exit(EXIT_FAILURE);
         }
 
+        if (killProcesses) {
 
-        if (killProcesses == 1) {
+            pthread_detach(thread1);
+            pthread_detach(thread2);
 
-            if (kill(cPid, SIGUSR1) == 0) {
-                printf("cPid %d off\n", cPid);
-            }
+            int k = kill(cPid, SIGUSR1);
+            printf("cPid %d %d\n", cPid, k);
             exit(EXIT_SUCCESS);
         }
     }
@@ -156,16 +174,18 @@ void shmB(long i) {
 
     // shmget returns an identifier in shmIdB
     shmIdB = shmget(shmKeyB, sizeof(struct MemoryB), 0666 | IPC_CREAT);
+
     if (shmIdB == -1) {
-        printf(" shmget error processB\n");
+        printf(" shmgetB error processB\n");
         exit(EXIT_FAILURE);
     } else {
         printf(" processB has received a shared memory %d\n", shmIdB);
     }
 
     shmPtrB = (struct MemoryB *) shmat(shmIdB, NULL, 0); // shmat to attach to shared memory.
+
     if ((long) shmPtrB == -1) {
-        printf(" shmat error processB\n");
+        printf(" shmatB error processB\n");
         exit(EXIT_FAILURE);
     } else {
         printf(" processB has attached the shared memory %p\n", shmPtrB);
@@ -185,7 +205,7 @@ void shmB(long i) {
         sleep(1);
     }
 
-    if (killProcesses == 1) {
+    if (killProcesses) {
         //printf(" processB has detected the completion of processC...\n");
 
         shmdt((void *) shmPtrB);
@@ -194,9 +214,9 @@ void shmB(long i) {
         shmctl(shmIdB, IPC_RMID, NULL);
         printf(" processB has removed its shared memory...\n");
 
-        if (kill(shmBpid, SIGUSR1) == 0) {
-            printf("shmBpid %d off\n", shmBpid);
-        }
+        int k = kill(shmBpid, SIGUSR1);
+        printf("shmBpid %d %d\n", shmBpid, k);
+
         exit(EXIT_SUCCESS);
     }
 }
@@ -210,77 +230,20 @@ void processB(long inputLong) {
     printf(" 'B' squaring result: %li\n", input_pow);
 
     if (input_pow == 100) {
-        killProcesses = 1;
-        printf("!kill all Processes!\n");
+        killProcesses = true;
+        printf("!-kill all Processes-!\n");
     }
 
     shmB(input_pow);
 }
 
-void aProcess(long in) {
+void aProcess() {
 
     // fd[0];   //-> for using read end
     // fd[1];   //-> for using write end
     int fd[2];
-    pid_t aPid, pid;
+    pid_t aPid = -1, pid;
     int status;
-
-    if (pipe(fd) == -1) {
-        perror("pipe fd failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    aPid = fork();
-
-    if (aPid == -1) {
-        perror("aPid fork failed\n");
-        exit(EXIT_FAILURE);
-    } else if (aPid > 0) {              // Parent process
-        close(fd[0]);                   // Close reading end of pipe
-
-        write(fd[1], &in, sizeof(in));  // Write input string
-
-        close(fd[1]);                   // close writing end of pipe
-
-        int ppipe_pid = getpid();
-        printf("Parent pipe pid: %d\n", ppipe_pid);
-
-        pid = wait(&status);
-        printf("aProcess detects process %d was done.\n", pid);
-
-        if (killProcesses == 1) {
-            wait(NULL);                     // Wait for child to send a string
-
-            printf("aProcess process done.\n");
-
-            if (kill(ppipe_pid, SIGUSR1) == 0) {
-                printf("ppipe_pid %d off\n", ppipe_pid);
-            }
-            exit(EXIT_SUCCESS);
-        }
-    } else if (aPid == 0) {             // child process
-        close(fd[1]);                   // Close writing end of pipe
-
-        long pipeRead = 0;
-        read(fd[0], &pipeRead, BUF_SIZE);   // Read a string using pipe
-
-        close(fd[0]);                   // Close reading end
-
-        int cpipe_pid = getpid();
-        printf("child pipe pid: %d\n", cpipe_pid);
-
-        processB(pipeRead);             // run process B
-
-        if (killProcesses == 1) {
-            if (kill(cpipe_pid, SIGUSR1) == 0) {
-                printf("cpipe_pid %d off\n", cpipe_pid);
-            }
-            exit(EXIT_SUCCESS);
-        }
-    }
-}
-
-void inputScan(void) {
     long a = 0;
     char in[BUF_SIZE];
 
@@ -288,36 +251,82 @@ void inputScan(void) {
         int inputScanPid = getpid();
         printf("inputScanPid: %d\n", inputScanPid);
 
-        int s = scanf("%s", in);      // user input
-        assert(s == 1);                 // only one 'line' allowed
+        int s = scanf("%s", in);        // user input
+        assert(s == 1);     // only one 'line' allowed
 
         long input_convert = strtol(in, NULL, 10);  // convert input string to 'long' number
         printf("input_convert: %li, len: %zu\n", input_convert, sizeof(input_convert));
 
         a = input_convert;
 
-        aProcess(a);                    // start PIPE method
+        if (pipe(fd) == -1) {
+            perror("pipe fd failed\n");
+            exit(EXIT_FAILURE);
+        }
 
-        if (killProcesses == 1) {
-            if (kill(inputScanPid, SIGUSR1) == 0) {
-                printf("inputScanPid %d off\n", inputScanPid);
+        pid = getpid();
+
+        if (aPid != pid) {
+            aPid = fork();          // start process A
+        }
+        printf("aPid: %d, pid: %d\n", aPid, pid);
+
+        if (aPid == -1) {
+            perror("aPid fork failed\n");
+            exit(EXIT_FAILURE);
+        } else if (aPid > 0) {              // Parent process
+            close(fd[0]);                   // Close reading end of pipe
+
+            write(fd[1], &a, sizeof(a));  // Write input string
+
+            close(fd[1]);                   // close writing end of pipe
+
+            int ppipe_pid = getpid();
+            printf("Parent pipe pid: %d\n", ppipe_pid);
+
+            pid = wait(&status);
+            printf("aProcess detects process %d was done.\n", pid);
+
+            if (killProcesses) {
+                wait(NULL);                     // Wait for child to send a string
+
+                printf("aProcess process done.\n");
+
+                int k = kill(ppipe_pid, SIGUSR1);
+                printf("ppipe_pid %d %d\n", ppipe_pid, k);
+
+                exit(EXIT_SUCCESS);
             }
+        } else if (aPid == 0) {             // child process
+            close(fd[1]);                   // Close writing end of pipe
+
+            long pipeRead = 0;
+            read(fd[0], &pipeRead, BUF_SIZE);   // Read a string using pipe
+
+            close(fd[0]);                   // Close reading end
+
+            int cpipe_pid = getpid();
+            printf("child pipe pid: %d\n", cpipe_pid);
+
+            processB(pipeRead);             // run process B
+
+            if (killProcesses) {
+                int k = kill(cpipe_pid, SIGUSR1);
+                printf("cpipe_pid %d %d\n", cpipe_pid, k);
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+        if (killProcesses) {
+            int k = kill(inputScanPid, SIGUSR1);
+            printf("inputScanPid %d %d\n", inputScanPid, k);
         }
     }
 }
 
 int main(int argc, char *argv[]) {
 
-    pid_t scanPid;
-    scanPid = fork();
-    printf("%d\n", getpid());
-
-    if (scanPid == -1) {
-        perror("scanPid fork failed\n");
-        exit(EXIT_FAILURE);
-    } else if (scanPid > 0) {
-        inputScan();
-    }
+    aProcess();
 
     printf("--- main(), close all ---\n");
 
